38b9792c5d682970a8a5421a2096a0d66034220f
Alexandro Delgado <alexandrodelgado@hotmail.com>
2021-06-01 12:35:18 -0300

    Atualizando Framework

--- APICompleta/src/DevIO.Data/Context/MeuDbContext.cs
+++ APICompleta/src/DevIO.Data/Context/MeuDbContext.cs
@@ -21,7 +21,7 @@ namespace DevIO.Data.Context
                 .SelectMany(e => e.GetProperties()
                     .Where(p => p.ClrType == typeof(string))))
                 property.Relational().ColumnType = "varchar(100)";
-
+;
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
 
             foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
